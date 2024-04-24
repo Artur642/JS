fdda8e36dd75674be7db454b83c125dba38b90f8
Artur642 <136634672+Artur642@users.noreply.github.com>
2024-04-24 19:39:42 +0200

    ооо

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+
+// const b = 10
+// a = b * 2;
+// 
+
+// let age = 18;
+// age = 19;
+// console.log(age);
+
+// let userName = "Anna";
+// console.log(userName);
+
+// 
+const message = prompt("Чи погоджуєтесь ви з умовами");
+console.log(message);
